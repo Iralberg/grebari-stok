c020dce80c7c5fb82e1c6b28fcfccb67ac80bf8d
iralberg <iralbergsampaio@gmail.com>
2026-05-19 19:20:24 -0300

    Update produtos.js

--- js/produtos.js
+++ js/produtos.js
@@ -127,7 +127,7 @@ function submitForm() {
     produtos[i] = { ...produtos[i], ...p };
     showToast('Produto atualizado!', 'success');
   } else {
-    produtos.push({ id:gerarId(), ...p });
+    produtos.push({ id:nextId++, ...p });
     showToast('Produto cadastrado!', 'success');
   }
   saveProdutos()
